36bba8ecc5946f87a14d44979a6aeb81538288dc
elarbi99 <elarbi_m@yahoo.com>
2025-03-22 22:43:27 -0400

    Updated script to capture keystrokes seperatly

--- capture_Image.py
+++ capture_Image.py
@@ -7,17 +7,33 @@ import os
 
 screenshotFolder = "screenshots"
 incrementNumberFile="index.txt"
+wLabelsFile="wLabels.txt"
+aLabelsFile="aLabels.txt"
+sLabelsFile="sLabels.txt"
+dLabelsFile="dLabels.txt"
 os.makedirs(screenshotFolder, exist_ok=True)
-def screenCapture(index):
+def screenCapture(index,fileName,keyPressed):
     with mss.mss() as sct:
         monitors=sct.monitors
         monitor=monitors[1]
         screenshot=sct.grab(monitor)
         mss.tools.to_png(screenshot.rgb,screenshot.size,output="screenshots/screenshot"+str(index)+".png")
-if not os.path.exists(incrementNumberFile) or os.stat(incrementNumberFile).st_size ==0:
-    fileWrite=open(incrementNumberFile,"w")
-    fileWrite.write("1")
+    fileWrite=open(fileName,"a")
+    fileWrite.write("screenshot"+str(i)+".png" + " Label: "+keyPressed+"\n")
     fileWrite.close()
+    
+
+def fileCheck(fileName):
+    if not os.path.exists(fileName) or os.stat(fileName).st_size ==0:
+        fileWrite=open(fileName,"w")
+        if fileName == "index.txt":   
+            fileWrite.write("1")
+        fileWrite.close()
+fileCheck(incrementNumberFile)
+fileCheck(wLabelsFile)
+fileCheck(aLabelsFile)
+fileCheck(sLabelsFile)
+fileCheck(dLabelsFile)
 fileRead=open(incrementNumberFile,"r")
 indexNumber=fileRead.read().strip()
 fileRead.close()
@@ -27,8 +43,20 @@ if indexNumber.isdigit():
 else:
     i=1
 while True:
-    if keyboard.is_pressed("w") or keyboard.is_pressed("a") or keyboard.is_pressed("s") or keyboard.is_pressed("d"):
-        screenCapture(i)
+    if keyboard.is_pressed("w"):
+        screenCapture(i,wLabelsFile,"w")
+        i+=1
+        time.sleep(0.2)
+    elif keyboard.is_pressed("a"):
+        screenCapture(i,aLabelsFile,"a")
+        i+=1
+        time.sleep(0.2)
+    elif keyboard.is_pressed("s"):
+        screenCapture(i,sLabelsFile,"s")
+        i+=1
+        time.sleep(0.2)
+    elif keyboard.is_pressed("d"):
+        screenCapture(i,dLabelsFile,"d")
         i+=1
         time.sleep(0.2)
     elif keyboard.is_pressed("esc"):
