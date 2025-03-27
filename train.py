c4a3fcb7d40869425ad019747659b24f151413e4
elkhanzada <ismayilzadeelxan@gmail.com>
2025-03-27 18:18:26 -0400

    updated train

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def validate(model, criterion, dataloader, device):
     total = 0
 
     with torch.no_grad():
-        for inputs, labels in dataloader:
+        for inputs, labels in tqdm(dataloader):
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
             loss = criterion(outputs, labels)
@@ -62,7 +62,17 @@ def main():
     num_classes = 7  # Adjust based on your dataset
 
     # Data transforms
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.RandomAffine(degrees=10, translate=(0.1, 0.1)),
+        transforms.RandomPerspective(distortion_scale=0.2, p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    val_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -70,11 +80,11 @@ def main():
     train_folder = "train"
     test_folder = "test"
     # Datasets and DataLoaders
-    train_dataset = datasets.ImageFolder(root=train_folder, transform=transform)
-    # val_dataset = datasets.ImageFolder(root=test_folder, transform=transform)
+    train_dataset = datasets.ImageFolder(root=train_folder, transform=train_transform)
+    val_dataset = datasets.ImageFolder(root=test_folder, transform=val_transform)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    # val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     # Load EfficientNet and modify the classifier
     model = efficientnet_b2(weights=True)
@@ -88,7 +98,7 @@ def main():
     for epoch in range(num_epochs):
         print(f'Epoch {epoch+1}/{num_epochs}')
         train(model, criterion, optimizer, train_loader, device)
-        # validate(model, criterion, val_loader, device)
+        validate(model, criterion, val_loader, device)
     torch.save(model.state_dict(), "model.pt")
 
 if __name__ == '__main__':
